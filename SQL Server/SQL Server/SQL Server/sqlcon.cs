971b7debcbe5bd3fdb117d73c838343117e8230c
Erwin007X <85892867+Erwin007X@users.noreply.github.com>
2022-01-29 15:21:41 +0700

    Add files via upload

---
+++ SQL Server/SQL Server/SQL Server/sqlcon.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.SqlClient;
+
+namespace SQL_Server
+{
+    public class sqlcon
+    {
+        public static SqlConnection conn { get; set; }
+        public static SqlCommand cmd { get; set; }
+        public static SqlDataReader dr { get; set; }
+        public static string cmdText { get; set; }
+    }
+}
